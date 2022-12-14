c05de9820105af7a89b6fe154d6d0705665139c3
Wellington Cruz <marcelinoc.wellington@gmail.com>
2022-12-14 09:43:52 -0300

    Alterações descartadas

---
+++ src/components/InputText.vue
@@ -0,0 +1,17 @@
+<template>
+    <label class="row" :for="id">
+        <!-- <p class="p-primario">Se inscreva na nossa newsletter e receba diariamente notícias e dicas de lugares para se
+        hospedar e promoções:</p> -->
+    </label>
+    <input class="input-padrao p-primario" type="text" :id="id" :placeholder="placeholder">
+</template>
+
+<script>
+export default {
+    name: "InputText",
+    props: {
+        id: String,
+        placeholder: String,
+    }
+}
+</script>
