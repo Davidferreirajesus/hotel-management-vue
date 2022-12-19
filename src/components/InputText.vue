efac5932d622046cc2c616b0540f300a4953bb73
kelvya <kelvya.kt@gmail.com>
2022-12-19 20:36:00 -0300

    Alterações de css, responsividade do component InputText

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -3,7 +3,7 @@
         <!-- <p class="p-primario">Se inscreva na nossa newsletter e receba diariamente notícias e dicas de lugares para se
         hospedar e promoções:</p> -->
     </label>
-    <input class="input-padrao p-primario" type="text" :id="id" :placeholder="placeholder">
+    <input class="input-padrao input-basico p-primario" type="text" :id="id" :placeholder="placeholder">
 </template>
 
 <script>
@@ -15,3 +15,32 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.input-padrao {
+    border: white solid 1px;
+    margin: 1rem;
+    width: 22vw;
+    height: 2.5rem;
+    text-align: center;
+    border-radius: 10px;
+    box-shadow: 1px 3px 5px #213242;
+}
+
+.input-basico {
+    border: white solid 1px;
+    margin: 0.5rem;
+    text-align: center;
+    border-radius: 10px;
+    box-shadow: 1px 3px 5px #213242;
+    width: 70vw;
+    height: 2.5rem;
+}
+
+.p-primario {
+    padding: 0 1.5rem;
+    text-align: start;
+    color: #213242;
+    font: "Quicksand";
+}
+</style>
