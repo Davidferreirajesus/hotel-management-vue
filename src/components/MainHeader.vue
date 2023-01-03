3e21b1d686f8b40eebfb31056febb37304d7388d
Andrey Mendes <andreylsm@gmail.com>
2023-01-03 12:36:22 -0300

    Criação modalLogin / Funções de login

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,16 +1,18 @@
 <template>
     <header class="header--container container-fluid text-end m-0 p-1">
+        <div class="text-center">
+            <span class="p-4" id="logado-usuario"></span>
+        </div>
         <div class="container" id="logado">
-            <span class="col p-1" id="logado-usuario"></span>
-            <button class="btn btn-secondary text-bg-secondary col p-1 ">
+            <button class="btn btn-secondary text-bg-secondary col p-1 " @click="sairLogin">
                 Sair
             </button>
         </div>
-        <!-- <div class="container" id="cadastro">
-            <button class="btn btn-secondary col p-1">
-                Cadastre-se
+        <div class="container" id="cadastro">
+            <button class="btn btn-secondary col p-1" data-bs-toggle="modal" data-bs-target="#modalLogin">
+                Login
             </button>
-        </div> -->
+        </div>
         <div class="d-flex align-items-center">
             <router-link to="/" class="header--logo" title="Grand Hoyal Hotel"></router-link>
             <div class="header--container2">
@@ -22,18 +24,54 @@
             </div>
         </div>
     </header>
+    <ModalLogin />
 </template>
 
 <script>
 import "@/assets/css/main.css"
 import "@/assets/css/header-footer.css"
+import "@/assets/css/login.css"
 
 import SocialMedia from '@/components/SocialMedia.vue'
+import ModalLogin from '@/components/ModalLogin.vue'
 
 export default {
     name: 'MainHeader',
     components: {
-        SocialMedia
+        SocialMedia,
+        ModalLogin
+    },
+    methods: {
+        sairLogin() {
+            localStorage.removeItem('login')
+            document.querySelector('#logado-usuario').innerText = ''
+            document.querySelector('#logado').classList.add('esconder')
+            document.querySelector('#cadastro').classList.remove('esconder')
+        }
+    },
+    mounted: function() {
+        let cadastros = []
+        cadastros = JSON.parse(localStorage.getItem('cadastros'))
+
+        // Validação de login realizado
+        if (localStorage.getItem('login')) {
+            let cadastroEmail =  localStorage.getItem('login')
+            // Varre a lista de cadastros
+            for (let i = 0; i < cadastros.length; i++) {
+                if (cadastros[i].email == cadastroEmail) {
+                    document.querySelector('#logado-usuario').innerText = `Bem-vindo ${cadastros[i].nome}`
+                    continue
+                }
+            }
+
+
+            document.querySelector('#logado').classList.remove('esconder')
+            document.querySelector('#cadastro').classList.add('esconder')
+        }
+        else {
+            document.querySelector('#logado').classList.add('esconder')
+            document.querySelector('#cadastro').classList.remove('esconder')
+        }
     }
 }
 </script>
---
+++ src/components/ModalLogin.vue
@@ -0,0 +1,138 @@
+<template>
+    <div>
+        <div class="modal fade" id="modalLogin" tabindex="-1" aria-labelledby="modalLoginLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modalLogin__body">
+                        
+                        <div class="modalLogin__container">
+                            <div class="modalLogin__box">
+                                <input id="item-1" type="radio" name="item" class="sign-in" checked><label for="item-1" class="item">Login</label>
+                                <input id="item-2" type="radio" name="item" class="sign-up"><label for="item-2" class="item">Cadastro</label>
+                                <div class="login-form">
+                                    <div class="sign-in-htm">
+                                        <div class="modalLogin__group">
+                                            <input placeholder="Email" id="loginEmail" type="email" class="input">
+                                        </div>
+                                        <div class="modalLogin__group">
+                                            <input placeholder="Senha" id="loginPass" type="password" class="input" data-type="password">
+                                        </div>
+
+                                        <div class="modalLogin__group">
+                                            <input type="submit" class="button" value="Entrar" @click="realizaLogin" data-bs-dismiss="modal">
+                                        </div>
+                                        <div class="hr"></div>
+                                        <div class="modalLogin__footer">
+                                            <a href="#forgot">Esqueceu a senha?</a>
+                                        </div>
+                                    </div>
+                                    <div class="sign-up-htm">
+                                        <div class="modalLogin__group">
+                                            <input placeholder="Usuário" id="cadastroUser" type="text" class="input">
+                                        </div>
+                                        <div class="modalLogin__group">
+                                            <input placeholder="Email" id="cadastroEmail" type="email" class="input">
+                                        </div>
+                                        <div class="modalLogin__group">
+                                            <input placeholder="Senha" id="cadastroPass" type="password" class="input" data-type="password">
+                                        </div>
+                                        <div class="modalLogin__group">
+                                            <input placeholder="Repita a senha" id="cadastroRepass" type="password" class="input" data-type="password">
+                                        </div>
+
+                                        <div class="modalLogin__group">
+                                            <input type="submit" class="button" value="Cadastrar" @click="cadastroLogin" data-bs-dismiss="modal">
+                                        </div>
+                                        <div class="hr"></div>
+                                        <div class="modalLogin__footer">
+                                            <label class="label" for="item-1">Já possui cadastro?</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ModalLogin",
+    data() {
+        return {
+            cadastros: []
+        }
+    },
+    methods: {
+        cadastroLogin () {
+            let cadastroUser = document.querySelector("#cadastroUser").value
+            let cadastroEmail = document.querySelector("#cadastroEmail").value
+            let cadastroPass = document.querySelector("#cadastroPass").value
+            // let cadastroRepass = document.querySelector("#cadastroRepass").value
+
+            // Cria o objeto cadastro
+            let cadastro = {}
+            cadastro.nome = cadastroUser
+            cadastro.email = cadastroEmail
+            cadastro.senha = cadastroPass
+
+            // Acrescenta o cadastro aos cadastros no localstorage
+            if(localStorage.getItem('cadastros')) {
+                this.cadastros = JSON.parse(localStorage.getItem('cadastros'))
+            }
+            this.cadastros.push(cadastro)
+            localStorage.setItem('cadastros', JSON.stringify(this.cadastros))
+
+            this.clearLogin()
+            // Realiza o login
+            localStorage.setItem('login', cadastroEmail)
+            document.querySelector('#logado').classList.remove('esconder')
+            document.querySelector('#cadastro').classList.add('esconder')
+            document.querySelector('#logado-usuario').innerText = cadastroUser
+
+        },
+        // Limpa os dados preenchidos do cadastro
+        clearLogin() {
+            document.querySelector("#cadastroUser").value = ''
+            document.querySelector("#cadastroEmail").value = ''
+            document.querySelector("#cadastroPass").value = ''
+            document.querySelector("#cadastroRepass").value = ''
+        },
+        realizaLogin() {
+            let loginEmail = document.querySelector("#loginEmail").value
+            let loginPass = document.querySelector("#loginPass").value
+            let valida = this.validaLogin (loginEmail, loginPass)
+
+            if (valida != -1) {
+                localStorage.setItem('login', loginEmail)
+                document.querySelector('#logado').classList.remove('esconder')
+                document.querySelector('#cadastro').classList.add('esconder')
+                document.querySelector('#logado-usuario').innerText = `Bem-vindo ${this.cadastros[valida].nome}`
+            } else {
+                alert ('Cadastro não encontrado!')
+            }
+            document.querySelector("#loginEmail").value = ''
+            document.querySelector("#loginPass").value = ''        
+
+            console.log(loginEmail, loginPass, valida)
+        },
+        // Valida se o login realizado está na lista de cadastros (email e senha)
+        validaLogin(loginEmail, loginPass) {
+            if(localStorage.getItem('cadastros')) {
+                this.cadastros = JSON.parse(localStorage.getItem('cadastros'))
+
+                // Varre a lista de cadastros
+                for (let i = 0; i < this.cadastros.length; i++) {
+                    if (this.cadastros[i].email == loginEmail && this.cadastros[i].senha == loginPass) {
+                        return i
+                    }
+                }
+            }
+            return -1
+        }
+    }
+}
+</script>
